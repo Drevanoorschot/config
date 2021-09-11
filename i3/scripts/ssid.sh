35ab3d6791a72ae8968debde53045b6d62345413
Drevanoorschot <d.h.m.a.vanoorschot@student.utwente.nl>
2021-09-11 21:39:11 +0200

    Make ssid regex "slightly" more robust
    it's a real pain to parse nmcli

--- i3/scripts/ssid.sh
+++ i3/scripts/ssid.sh
@@ -2,7 +2,7 @@ RED='<span foreground="#FF0000">'
 WHITE='<span foreground="#ffa100">'
 SUFFIX='</span>'
 DOWN='down'
-SSID=$(nmcli connection | grep wlan0 | grep -o '^\S\S\S\S*') # who has more than 5 spaces in their SSID?
+SSID=$(nmcli connection | grep wlan0 | awk -F '  ' '{print $1}') # who has more than 5 spaces in their SSID?
 if [ -n "$SSID" ]; then
     echo $WHITE$SSID$SUFFIX
     exit
